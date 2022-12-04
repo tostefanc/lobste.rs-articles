9400912b2ee897fea10a8b570cfa9e9cfece26b2
Stefan <stefan.toma100@gmail.com>
2022-12-04 21:38:27 +0200

    code housekeeping

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python3
 
-from bs4 import BeautifulSoup as bs
 import lobster_req
 import os.path as path
 import json
 import time
-# 1. Make separate files with functions 
-# 2. Write to file the json 
-#       a. Check creation time, if longer than 6 hours delete the file and recreate it
-
 
 def is_articles_json_old(days=1): 
     articles_file = path.getmtime("articles.json") 
-    # Check against 24 hours 
+    
     if not (time.time() - articles_file) / 3600 > 24*days:
         soup_object = lobster_req.format_lobster_articles()
         json_soup = json.dumps(soup_object)
--- send_email.py
+++ send_email.py
@@ -2,7 +2,6 @@
 
 import smtplib
 import ssl
-from email.message import EmailMessage
 import sec #Local secrets
 import json
 from datetime import datetime as dt
@@ -21,28 +20,15 @@ with open('articles.json') as user_file:
 articles_dict = json.loads(articles_contents)
 
 for key, article in articles_dict.items():
-    # print(f"KEY: {key}  Titles: {article['title']} LINKS: {article['link']}")
     body += f"<p>{key}. <a href={article['link']}>{article['title']}</a></p>"
 
-# print(json.dumps(articles_dict, indent = 4,))
-
-# print( type(body))
 
 def send_the_lobster_articles(body_contents):
     em = MIMEMultipart()
     em['From'] = sec.email_sender
     em['To'] = sec.email_receiver
     em['Subject'] = subject_of_email
-    # em.add_header('Content-Type','text/html')
     em.attach(MIMEText(body, "html"))
-    
-    # em = EmailMessage()
-    # em['From'] = sec.email_sender
-    # em['To'] = sec.email_receiver
-    # em['Subject'] = subject_of_email
-    # em.add_header('Content-Type','text/html')
-    # em.set_content("{}".format(body_contents))
-    # em.set_payload(body_contents)
 
     context = ssl.create_default_context()
 
