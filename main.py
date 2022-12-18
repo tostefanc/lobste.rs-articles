ae8a988b84856b6b3946f7dd7afa155c5729dd3a
Stefan Toma <stefan@Stefans-MBP.mbrv>
2022-12-18 22:04:14 +0200

    format main and extra gitignore

--- main.py
+++ main.py
@@ -8,16 +8,18 @@ from send_email import send_the_lobster_articles
 
 articles_file = "articles.json"
 
+
 def is_articles_json_old(days=1): 
     articles_file_timestamp = os.path.getmtime(articles_file) 
     if (time.time() - articles_file_timestamp) / 3600 > 24*days:
         return True
 
+
 def recreate_articles_json():
     os.remove(articles_file)
     soup_object = lobster_req.format_lobster_articles()
     json_soup = json.dumps(soup_object)
-    with open("articles.json",'w',encoding = 'utf-8') as f:
+    with open("articles.json", 'w', encoding='utf-8') as f:
         f.write(json_soup)
         f.close
 
